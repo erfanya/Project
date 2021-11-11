88a458392b5deddc018708ea9a17b332420d0676
erfanya <92828785+erfanya@users.noreply.github.com>
2021-11-11 18:52:28 +0330

    Add files via upload

---
+++ src/Doctor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+#include "person.h"
+#include "doctor.h"
+
+using namespace std;
+
+Doctor::~Doctor()
+{}
+
+void Doctor::set_id(int id)
+{
+     if (id > 0 && id <= 100)
+     {
+          this->id = id;
+     }
+     else
+     {
+          cout << "Enter a valid ID Number (1-100)" << endl;
+     }
+}
+
+void Doctor::set_specialization(Specialization specialization)
+{
+     this->specialization = specialization;
+}
+
+int Doctor::get_id() const
+{
+     return id;
+}
+
+Doctor::Specialization Doctor::get_specialization() const
+{
+     return specialization;
+}
