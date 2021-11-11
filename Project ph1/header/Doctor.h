967f039c50ed3890d6b2fa8e1f0d2eaba49a6c83
erfanya <92828785+erfanya@users.noreply.github.com>
2021-11-11 18:49:23 +0330

    Add files via upload

---
+++ Project ph1/header/Doctor.h
@@ -0,0 +1,32 @@
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+#include <string>
+
+class Doctor
+{	
+	public:
+          // enum
+          enum Specialization {General, Expert, Surgeon, Unknown};
+          
+          // Constructor
+          Doctor(int id = 0, Specialization specialization = {Unknown});
+          Doctor(const Doctor&);//coppy constructor
+
+          //Destructor
+		~Doctor();
+
+          //set and get functions
+          void set_id(int);
+		void set_specialization(Specialization);
+        
+	     int get_id() const;
+          Specialization get_specialization() const;
+        
+     private:
+          //Data members      
+          int id; 
+          Specialization specialization;// takhasos 
+};
+
+#endif
---
+++ Project ph1/src/Visit.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include "visit.h"
+#include "person.h"
+#include "patient.h"
+#include "doctor.h"
+
+using namespace std;
+
+Visit::~Visit()
+{}
+
+void Visit::set_visit_time(Visit_Time visit_time)
+{
+     this->visit_time = visit_time;
+}
+
+void Visit::set_room(int room)
+{
+     this->room = room;
+}
+
+//void Visit::set_doctor()
+//{}
+
+//void Visit::set_patient()
+//{}
+
+void Visit::set_cost(Cost cost)
+{
+     this->cost = cost;
+}
+
+Visit::Visit_Time Visit::get_visit_time() const
+{
+     return visit_time;
+}
+
+int Visit::get_room() const
+{
+     return room;
+}
+
+//Visit::get_doctor() const
+//{}
+
+//Visit::get_patient() const
+//{}
+
+Visit::Cost Visit::get_cost() const
+{
+     return cost;
+}
